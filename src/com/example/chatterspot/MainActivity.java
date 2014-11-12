09680c6fadeef82f7e555dda732c8ed723afd211
Jordan Heier <heierj@uw.edu>
2014-11-11 18:39:19 -0800

    Very basic messages view done.

--- src/com/example/chatterspot/MainActivity.java
+++ src/com/example/chatterspot/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.chatterspot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.example.chatterspot.MESSAGE";
+	private List<String> message = new ArrayList<String>();
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, message);
+		ListView messages = (ListView) findViewById(R.id.messages);
+		messages.setAdapter(adapter);
 	}
 
 	@Override
@@ -31,4 +44,11 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		adapter.add(editText.getText().toString());
+	}
+	
 }
