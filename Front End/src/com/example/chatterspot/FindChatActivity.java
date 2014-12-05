c538ec85d8a792c3af0ab88e20bdec97f3b15473
Jordan Heier <heierj@uw.edu>
2014-12-05 13:09:29 -0800

    Refactoring and and fix some location bugs

--- Front End/src/com/example/chatterspot/FindChatActivity.java
+++ Front End/src/com/example/chatterspot/FindChatActivity.java
@@ -16,7 +16,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -26,14 +25,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class FindChatActivity extends Activity implements LocationListener, AdapterView.OnItemClickListener {
+public class FindChatActivity extends Activity implements AdapterView.OnItemClickListener {
 	private static final int CHATROOM_RADIUS = 50;
 	public final static String CHAT = "com.example.chatterspot.CHAT";
 	private ArrayList<Chatroom> chats;
+	private FindChatLocationManager locationManager;
+	private boolean locationSet;
 	private ChatroomClient client;
 	private ChatAdapter adapter;
-	private LocationManager locationManager;
-	private Location lastLocation;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +45,9 @@ public class FindChatActivity extends Activity implements LocationListener, Adap
 		client = new ChatroomClient(this);
 		
 		// Prepare location services
-		setupLocationManager();
-		lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if (lastLocation == null) {
-			lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		}
+		locationSet = false;
+		locationManager = new FindChatLocationManager((LocationManager) getSystemService(Context.LOCATION_SERVICE),
+				this);
 		
 		getActionBar().setTitle("Explore Chats");
 		 
@@ -59,11 +56,6 @@ public class FindChatActivity extends Activity implements LocationListener, Adap
 		ListView chatView = (ListView) findViewById(R.id.chats);
 		chatView.setAdapter(adapter);
 		chatView.setOnItemClickListener(this);
-	}
-
-	private void setupLocationManager() {
-	    locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, this);
 	}
 
 	@Override
@@ -89,84 +81,108 @@ public class FindChatActivity extends Activity implements LocationListener, Adap
 	 */
 	public void addChatrooms(List<Chatroom> chatrooms) {
 		if(chatrooms == null) return;
-		for(Chatroom chatroom : chatrooms) {
-			chats.add(chatroom);
-			ChatroomUtils.setDistanceInFeet(chats, lastLocation);
-			Collections.sort(chats, new ChatroomComparator());
-			adapter.notifyDataSetChanged();
-		}
+		chats.addAll(chatrooms);
+		setNewLocation(locationManager.getLocation());
 	}
 	
+	/**
+	 * Updates the location of the user in the UI
+	 * @param location
+	 */
+	public void setNewLocation(Location location) {
+		if(location == null) {
+			adapter.notifyDataSetChanged();
+			return;
+		} else {
+			locationSet = true;
+		}
+		ChatroomUtils.setDistanceInFeet(chats, location);
+		Collections.sort(chats, new ChatroomComparator());
+		adapter.notifyDataSetChanged();
+	}
+	
+	/**
+	 * Pause location updates
+	 */
+	@Override
+	public void onPause() {
+		super.onPause();
+		locationSet = false;
+		locationManager.pauseUpdates();
+	}
+
+	/**
+	 * Resume location updates
+	 */
 	@Override
 	public void onResume() {
 	    super.onResume();  // Always call the superclass method first
-	    checkLocationService();
-	}
-	
-	private void checkLocationService() {
-		boolean gps_enabled = false;
-		try{
-	    	gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-	    } catch(Exception ex){}
-
-	   if(!gps_enabled){
-	        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-	        dialog.setMessage(this.getResources().getString(R.string.gps_network_not_enabled));
-	        dialog.setPositiveButton(this.getResources().getString(R.string.open_location_settings), 
-	        		new DialogInterface.OnClickListener() {
-			            @Override
-			            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-			                Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS );
-			                startActivity(myIntent);
-			                //get gps
-			            }
-	        		});
-	        dialog.setNegativeButton(this.getString(R.string.cancel), 
-	        		new DialogInterface.OnClickListener() {
-			            @Override
-			            public void onClick(DialogInterface paramDialogInterface, int paramInt) {}
-	        		});
-	        dialog.show();
+	    locationManager.resumeUpdates();
+	    if(!locationManager.gpsEnabled()) {
+	    	showGpsDialog();
 	    }
+	    setNewLocation(locationManager.getLocation());
 	}
 	
+	/**
+	 * Shows a dialag to the user letting them know they need to have
+	 * GPS services turned on to use the application
+	 */
+	private void showGpsDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(this.getResources().getString(R.string.gps_network_not_enabled));
+        dialog.setPositiveButton(this.getResources().getString(R.string.open_location_settings), 
+        		new DialogInterface.OnClickListener() {
+		            @Override
+		            public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+		                Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS );
+		                startActivity(myIntent);
+		                //get gps
+		            }
+        		});
+        dialog.setNegativeButton(this.getString(R.string.cancel), 
+        		new DialogInterface.OnClickListener() {
+		            @Override
+		            public void onClick(DialogInterface paramDialogInterface, int paramInt) {}
+        		});
+        dialog.show();
+	}
+	
+	/**
+	 * Sends a newly created chat to the server
+	 */
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	  super.onActivityResult(requestCode, resultCode, data);
 	  if(resultCode == Activity.RESULT_OK) {
 		  String chatName = data.getStringExtra(CreateChatActivity.CHAT_NAME);
 		  if(chatName == null) return;
+		  Location lastLocation = locationManager.getLocation();
+		  
 		  if(lastLocation == null) {
-			  lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+			  showGpsDialog();
+			  return;
 		  }
+		  
 		  Chatroom chat = new Chatroom(chatName, null, resultCode, lastLocation.getLatitude(), lastLocation.getLongitude());
 		  System.out.println("Chat: " + chat.getName());
 		  client.createChat(chat);
 	  }
 	}
 
-	@Override
-	public void onLocationChanged(Location location) {
-		lastLocation = location;
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-	}
-
+	/**
+	 * Handles the event when a user is trying to join a chat
+	 */
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 	    Chatroom clickedChat = chats.get(position);
-	    if(clickedChat.getCurDist() < CHATROOM_RADIUS) {
+	    if (!locationManager.gpsEnabled()) {
+	    	showGpsDialog();
+	    } else if (!locationSet){
+	    	// TODO: create a new dialog here
+	    	showGpsDialog();
+	    } else if(clickedChat.getCurDist() < CHATROOM_RADIUS) {
 		    Intent intent = new Intent(FindChatActivity.this, ChatActivity.class);
 		    intent.putExtra(CHAT, new Gson().toJson(clickedChat));
 		    startActivity(intent);
---
+++ Front End/src/com/example/chatterspot/FindChatLocationManager.java
@@ -0,0 +1,77 @@
+package com.example.chatterspot;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+/**
+ * This class is to help manage location updates for the 
+ * FindChatActivity
+ */
+public class FindChatLocationManager implements LocationListener {
+	private LocationManager locationManager;
+	private Location lastLocation;
+	private boolean providerEnabled;
+	private FindChatActivity activity;
+
+	public FindChatLocationManager(LocationManager locationManager, FindChatActivity activity) {
+		this.locationManager = locationManager;
+		this.activity = activity;
+	    resumeUpdates();
+	}
+	
+	/**
+	 * Sets the new location and updates it in the UI
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+		lastLocation = location;
+		activity.setNewLocation(location);
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		providerEnabled = true;
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		providerEnabled = false;
+	}
+	
+	/**
+	 * Used to pause updates when the user is not actively on the screen
+	 */
+	public void pauseUpdates() {
+		locationManager.removeUpdates(this);
+	}
+	
+	public void resumeUpdates() {
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, this);
+	}
+	
+	/**
+	 * @return the last known location. If gps doesn't have location yet then
+	 * return the coarse location
+	 */
+	public Location getLocation() {
+		if(!providerEnabled) {
+			return null;
+		}
+		if(lastLocation == null) {
+			lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		}
+		return lastLocation;
+	}
+
+	public boolean gpsEnabled() {
+	    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	}
+}
